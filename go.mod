fd730f783f8dcc9204d8eea936a21e6a1add79c2
Taliesin Sisson <taliesins@yahoo.com>
2021-01-20 12:29:32 +0000

    Move proto files to v1 and v2 folders

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/segmentio/ksuid v1.0.3
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 )
--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"context"
-	grakn "github.com/taliesins/client-go/grakn_protocol_v1"
-	grpc "google.golang.org/grpc"
-	"log"
-	"time"
+	"github.com/taliesins/client-go/v2"
 )
 
 func main() {
-	address := ":48555"
-	username := ""
-	password := ""
-
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-
-	// Set up context
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	client := grakn.NewKeyspaceServiceClient(conn)
-
-	databaseAllResult, err := client.Retrieve(ctx, &grakn.Keyspace_Retrieve_Req{
-		Username: username,
-		Password: password,
-	})
-	if err != nil {
-		log.Fatalf("could not list databases: %v", err)
-	}
-	log.Printf("databases: %v", databaseAllResult.Names)
+	v2.Run()
 }
